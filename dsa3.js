5e03d99c4e12844f5d154d712c1990a447ec7e09
akshay jain <123448064+AJAKSHAY77@users.noreply.github.com>
2023-07-11 01:14:09 +0530

    delete element from array

--- dsa3.js
+++ dsa3.js
@@ -1,6 +1,26 @@
+const array = [90,89,65,4,78,12,18,45,67,89]
+// const position = 6
+// for(i=position;i<array.length-1;i++){
+//     array[i] = array[i+1]
+//     console.log( i,array[i]);
+
+// }
+// array.length = array.length-1
+// console.log(array);
 
 
+function delete_element(){
+    const output = document.getElementById("output")
+    const userinput = document.getElementById("position").value
+    for(i=userinput;i<array.length-1;i++){
+        array[i] = array[i+1]
 
-const arr = [1,2,4,5,6,7,8,9]
-console.log(arr.slice(1,3));
-console.log(arr);
+    }
+
+    array.length = array.length-1
+    
+    
+
+    output.innerHTML = `after deleting array is ${array}`
+}
+
